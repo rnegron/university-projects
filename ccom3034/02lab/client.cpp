f7db0d576a1aa2e0cf12764077cb317da86589df
rucury <rucury@gmail.com>
2014-09-24 09:09:01 -0400

    Add comments and UI to lab02 client

--- ccom3034/02lab/client.cpp
+++ ccom3034/02lab/client.cpp
@@ -3,25 +3,46 @@
 
 using namespace std;
 
-const int SIZE = 9;
-
 int main() {
-	SweetArray A(SIZE), B(SIZE), C(SIZE);
 
-	for (int i=0; i<SIZE; i++) {
-		A[i] = i * 2;
-		B[i] = i * 4;
+	int sizeA, sizeB;
+	cout << "Hello Mr. TA! Please enter a size for the first array (A) > ";
+	cin >> sizeA;
+	cout << endl << "And now for the second array (B) > ";
+	cin >> sizeB;
+	cout << endl;
 
+	SweetArray A(sizeA);
+	SweetArray B(sizeB);
+
+	for (int i = 0; i < sizeA; i++) {
+		cout << "Enter an integer value for A[" << i << "] > ";
+		cin >> A[i];
+		cout << endl;
 	}
+
+	cout << "\tOkay! Array A is full." << endl;
+
+	for (int i = 0; i < sizeB; i++) {
+		cout << "Enter an integer value for B[" << i << "] > ";
+		cin >> B[i];
+		cout << endl;
+	}
+
+	cout << "\tOkay! Array B is full." << endl;
 
 	cout << "\nContents of A are: "  << A << endl;
 	cout << "Contents of B are: "  << B << endl << endl;
-
+	cout << "Press Enter to continue > ";
+	cin.get();
+	cin.ignore();
 
 
 	cout << "The sum of A and B is: " << (A + B) << endl << endl;
 	cout << "The intersection of A and B is: " << (A & B) << endl << endl;
 	cout << "The union of A and B is: " << (A | B) << endl << endl;
+
+	cout << "That's all! Have a good day." << endl;
 
 
 	return 0;
