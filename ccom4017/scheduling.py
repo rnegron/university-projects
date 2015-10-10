c352ae05a81c82be179fbcfa55da19cc174628c2
rnegron <rucury@gmail.com>
2015-10-09 23:09:04 -0400

    Add RR scheduling
    TODO
    - General cleanup
    - FCFS sorted output
    - Check and add comments

--- ccom4017/scheduling.py
+++ ccom4017/scheduling.py
@@ -1,29 +1,86 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Raúl E. Negrón
+# 801-13-4680
+# CCOM4017
+# scheduling.py
+
+
+"""
+TODO
+- General cleanup
+- FCFS sorted output
+- Check and add comments
+"""
+
+# Global, boilerplate output string
+OUTPUT = 'Process #{} took {} seconds.\n'
+PROC_LIST = [5, 9, 2, 0, 5]
+QUANTUM = 4
 
 def first_come(processes):
-    print('\tFirst come, first served\n')
+    print('--- First come, first served ---\n')
     acumulator = 0
+    # Go through every process in the list.
     for i in range(len(processes)):
+        # Use acumulator to keep track of the job times and simply
+        # output them as they come.
         acumulator += processes[i]
-        print('Process #{} took {} seconds.\n'.format(i + 1, acumulator))
+        print(OUTPUT.format(i + 1, acumulator))
 
 def shortest_job(processes):
-    print('\tShortest job first\n')
-    process_tuples = [(process + 1, time) for process, time in enumerate(processes)]
-    sorted_tuples = sorted(process_tuples, key=lambda time: time[1])
+    print('--- Shortest job first ---\n')
     acumulator = 0
+    # proc_pairs is a list of tuples of the form: (process number, CPU time).
+    proc_pairs = [(proc + 1, time) for proc, time in enumerate(processes)]
+
+    # Sort the proc_pairs by their second component (time) and assign
+    # the new list to sorted_tuples.
+    sorted_tuples = sorted(proc_pairs, key = lambda time: time[1])
+
     for i in range(len(processes)):
+        # Go through every process in the list and acumulate their job time,
+        # which is the second component of the ith tuple. Then output.
         acumulator += sorted_tuples[i][1]
-        print('Process #{} took {} seconds.\n'.format(sorted_tuples[i][0], acumulator))
+        print(OUTPUT.format(sorted_tuples[i][0], acumulator))
 
 def round_robin(processes, quantum):
-    print('\tRound Robin\n')
-    pass
+    print('--- Round robin ---\n')
+    acumulator = 0
+    proc_nest = [[proc + 1, time] for proc, time in enumerate(processes)]
+    proc_table = {proc + 1 : 0 for proc in range(len(processes))}
+
+    # A list in a boolean context is true only if it has at least one
+    # element. This loop runs until proc_pairs is empty.
+    while True:
+        proc_changed = False
+        for i in range(len(processes)):
+            if proc_nest[i][1] == 0:
+                continue
+
+            elif proc_nest[i][1] <= quantum:
+                acumulator += proc_nest[i][1]
+                proc_table[i + 1] += acumulator
+                proc_nest[i][1] = 0
+                proc_changed = True
+
+            else:
+                acumulator += quantum
+                proc_table[i + 1] += acumulator
+                proc_nest[i][1] -= quantum
+                proc_changed = True
+
+        if not proc_changed:
+            break
+
+    for proc, time in proc_table.iteritems():
+        print(OUTPUT.format(proc, time))
 
 def main():
-    process_list = [5, 1]
-    shortest_job(process_list)
-    first_come(process_list)
+    shortest_job(PROC_LIST)
+    first_come(PROC_LIST)
+    round_robin(PROC_LIST, QUANTUM)
 
 if __name__ == '__main__':
     main()
