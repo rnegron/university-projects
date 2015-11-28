29203b26c512f91f78750f8517fcbcbc998dd3bd
rnegron <rucury@gmail.com>
2015-11-28 12:18:31 -0400

    Add final page replacement algorithm
    Optimal and Second are now commented and ready for grading.

--- ccom4017/page-algo/optimal.py
+++ ccom4017/page-algo/optimal.py
@@ -11,30 +11,44 @@ memory = []
 
 
 def optimize(current_page, future_pages):
+    """
+    Given the page needed to insert and the future page requests,
+    this function will find the best page to evict from memory and
+    perform the necessary actions.
+    """
+
+    # If there are no more page requests after the current one,
+    # it does not matter which page is evicted from memory.
     if len(future_pages) == 0:
-        # print 'Since there are no more future requests, arbirtarily removing
-        # the oldest page in memory'
+        #! print 'Since there are no more future requests, arbirtarily removing the oldest page in memory'
         memory.pop(0)
         memory.append(current_page)
 
     else:
-        # print 'Need to put {} in {} and checking {}'.format(current_page,
-        # memory, future_pages)
-        memory_dict = {page: None for page in memory}
+        #! print 'Need to put {} in {} and checking {}'.format(current_page, memory, future_pages)
 
+        # Dictionary of the form PAGE:BIGGEST_INDEX_IN_FUTUE_PAGES
+        memory_dict = {page:None for page in memory}
+
+        # For every page in a copy of memory, we must check
+        # if it is a possible candidate for removal
         for page in memory[:]:
             if page not in future_pages:
-                # print 'Since its never used again, removing', page
+                #! print 'Since its never used again, removing', page
                 memory.remove(page)
                 return
 
             else:
+                # If the page is used in the future, mark its index
+                # and save it in the dictionary
                 idx = future_pages.index(page)
                 memory_dict[page] = idx
 
+        # The optimal page to remove from memory is the one whose index
+        # in the future page list is the max.
         optimal = max(memory_dict, key=memory_dict.get)
 
-        # print "Optimally, I should remove", optimal
+        #! print "Optimally, I should remove", optimal
         memory.remove(optimal)
 
 
@@ -43,25 +57,27 @@ def main():
     FILE = argv[2]
     page_faults = 0
 
+    # Open file and parse input
     with open(FILE, "r") as INPUT:
         memory_accesses = INPUT.read().split()
         page_requests = [page.split(":")[1] for page in memory_accesses]
 
+    # Loop through every page request and add them to memory or deal with
+    # the page fault optimally
     for idx, page in enumerate(page_requests):
         if page not in memory:
-            # print 'Page Fault #{} with page: {}'.format(page_faults + 1,
-            # page)
+            #! print 'Page Fault #{} with page: {}'.format(page_faults + 1, page)
             if len(memory) >= PAGE_AMOUNT:
                 optimize(page, page_requests[idx + 1:])
 
             memory.append(page)
             page_faults += 1
 
-    print 'PF:{}'.format(page_faults)
+    # Finally, output the number of page faults that occured
+    print 'Page Faults:{}'.format(page_faults)
 
 if __name__ == "__main__" and len(argv) == 3:
     main()
 
 else:
-    print(
-        "Usage: $ python %s <Number of memory page requests> <access file>" % argv[0])
+    print("Usage: $ python %s <Number of memory page requests> <access file>" % argv[0])
--- ccom4017/page-algo/second.py
+++ ccom4017/page-algo/second.py
@@ -8,21 +8,36 @@
 
 from sys import argv
 memory = []
-page_dict = {}
+page_dict = {} # Dictionary of the form PAGE:R_BIT
 
 
 def second_chance(current_page):
-    # print 'Trying to insert page {} into {}'.format(current_page, memory)
+    """
+    Given the page needede to insert into memory, this function will
+    search for a page to remove and make room by checking the R bits
+    of the pages in memory and re-arranging them as necessary.
+
+    The function will return a bool value since it used many times inside
+    a conditional loop.
+    """
+    #! print 'Trying to insert page {} into {}'.format(current_page, memory)
+
+    # For every page in a copy of memory, we must check
+    # if it is a possible candidate for removal
     for page in memory[:]:
+
+        # If the page has R == 1, then we must clear the bit and put the page
+        # at the tail of the list
         if page_dict[page] == 1:
-            # print 'Clearing R bit from page', page
+            #! print 'Clearing R bit from page', page
             page_dict[page] = 0
             memory.remove(page)
             memory.append(page)
 
+        # If the page has R == 0, then that page should be removed to make
+        # space for current_page.
         else:
-            # print 'Removing page {} to insert page {}'.format(page,
-            # current_page)
+            #! print 'Removing page {} to insert page {}'.format(page, current_page)
             memory.remove(page)
             memory.append(current_page)
             page_dict[current_page] = 1
@@ -35,31 +50,36 @@ def main():
     FILE = argv[2]
     page_faults = 0
 
+    # Open file and parse input
     with open(FILE, "r") as INPUT:
         memory_accesses = INPUT.read().split()
         page_requests = [page.split(":")[1] for page in memory_accesses]
 
+    # Loop through every incoming page request
     for page in page_requests:
+        # If the page is not in memory, it will trigger a page fault
         if page not in memory:
-            # print 'Page Fault #{} with page: {}'.format(page_faults + 1,
-            # page)
+            page_faults += 1
+            #! print 'Page Fault #{} with page: {}'.format(page_faults, page)
+
+            # If there is no room in memory, run the second chance algorithm
             if len(memory) >= PAGE_AMOUNT:
                 page_fault_fixed = False
+
+                # The algorithm will run until page_fault_fixed is True
                 while not page_fault_fixed:
                     page_fault_fixed = second_chance(page)
 
-                page_faults += 1
-
+            # If there is sufficient room in memory, just place the page in
             else:
-                page_dict[page] = 1
+                page_dict[page] = 1 # Set R bit of incoming page
                 memory.append(page)
-                page_faults += 1
 
-    print "PF:{}".format(page_faults)
+    # Finally, output the number of page faults that occured
+    print "Page Faults:{}".format(page_faults)
 
 if __name__ == "__main__" and len(argv) == 3:
     main()
 
 else:
-    print(
-        "Usage: $ python %s <Number of memory page requests> <access file>" % argv[0])
+    print("Usage: $ python %s <Number of memory page requests> <access file>" % argv[0])
